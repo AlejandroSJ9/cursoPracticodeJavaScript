492071f86fa0717c9fcedcf9e28fd9340675db72
AlejandroSJ9 <alejandro.salas090603@gmail.com>
2022-04-21 20:14:43 -0500

    "perimetro y area"

--- figuras.js
+++ figuras.js
@@ -1,1 +1,5 @@
-console.log("Hola mundo")
+const ladoCuadrado = 5;
+console.log("Los lados miden "+ladoCuadrado)
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetros es: "+perimetroCuadrado);
